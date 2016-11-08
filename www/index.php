61f46d2991d96fbf888a2af8552fb59b1dc0356c
Andrei Timotin <andreitimotin11@gmail.com>
2016-11-08 19:22:06 +0200

    wrote form

---
+++ www/index.php
@@ -0,0 +1,21 @@
+<html>
+<head>
+	<title>
+		Calculator
+	</title>
+</head>
+<body>
+	<h1>Calculator</h1>
+	<form action="index.php" method="post">
+		<input type="text" name="v1">
+		<input type="text" name="v2">
+		<select>
+			<option>+</option>
+			<option>-</option>
+			<option>*</option>
+			<option>/</option>
+			<option>!</option>
+		</select>
+		<input type="text" name="res">
+	</form>
+</body>
